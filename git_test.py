68b22a817311c900da22b751d3165f68cee92e78
kevin olpadwala <kevinmukeshbhai.olpadwala@edu.turkuamk.fi>
2026-03-31 12:59:56 +0300

    Resolved merge conflict in git_test.py after applying stash

--- git_test.py
+++ git_test.py
@@ -1,1 +1,6 @@
+<<<<<<< Updated upstream
 print("This is main branch change")
+=======
+print("This is main branch change")
+print("in dev branch I write code on the third row")
+>>>>>>> Stashed changes
